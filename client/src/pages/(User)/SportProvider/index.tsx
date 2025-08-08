3927ed9665a0344f239bdb6ac5fceb95a17295af
quangnguyentan <tanquang1510@gmail.com>
2025-08-08 11:04:00 +0700

    update code

--- client/src/pages/(User)/SportProvider/index.tsx
+++ client/src/pages/(User)/SportProvider/index.tsx
@@ -1,15 +1,51 @@
 import SportSection from "@/components/layout/SportSection";
 import belt_bottom_top from "@/assets/user/1330t190.gif";
-import { FootballIcon } from "@/components/layout/Icon";
+import {
+  ChevronRightIcon,
+  FootballIcon,
+  HomeIconSolid,
+} from "@/components/layout/Icon";
 import * as React from "react";
 import SchedulePage from "@/components/layout/SchedulePage";
 import { useScheduleData, formatDate } from "@/data/mockScheduleData";
 import { DataProvider, useData } from "@/context/DataContext";
-import { useParams } from "react-router-dom";
-import { MatchStatusType } from "@/types/match.types";
+import { useNavigate, useParams } from "react-router-dom";
+import { Match, MatchStatusType } from "@/types/match.types";
 import { adjustToVietnamTime } from "@/lib/helper";
 import { Loader } from "@/components/layout/Loader";
 import { Banner } from "@/types/banner.types";
+import { useSelectedPageContext } from "@/hooks/use-context";
+
+const Breadcrumbs: React.FC<{
+  sportName?: string;
+}> = ({ sportName }) => {
+  const navigate = useNavigate();
+  const { setSelectedSportsNavbarPage, setSelectedPage } =
+    useSelectedPageContext();
+  return (
+    <nav
+      className="text-xs text-gray-400 mb-2 px-1 flex items-center space-x-1.5 pt-4 pb-2"
+      aria-label="Breadcrumb"
+    >
+      <div
+        onClick={() => {
+          localStorage.removeItem("selectedSportsNavbarPage");
+          setSelectedSportsNavbarPage("");
+          localStorage.setItem("selectedPage", "TRANG CHỦ");
+          setSelectedPage("TRANG CHỦ");
+          navigate("/"); // Navigate to homepage
+        }}
+        className="hover:text-yellow-400 flex items-center text-xs text-white hover:text-xs cursor-pointer"
+      >
+        <HomeIconSolid className="w-3.5 h-3.5 mr-1" /> Trang chủ
+      </div>
+      <ChevronRightIcon className="w-3 h-3 text-gray-500 " />
+      <div className="text-xs text-current-color hover:text-xs cursor-pointer">
+        {sportName || "Thể thao"}
+      </div>
+    </nav>
+  );
+};
 
 const AppContent: React.FC = () => {
   const {
@@ -24,7 +60,7 @@ const AppContent: React.FC = () => {
   const today = React.useMemo(() => new Date(), []);
   const vietnamToday = React.useMemo(() => adjustToVietnamTime(today), [today]);
   const containerRef = React.useRef<HTMLDivElement>(null);
-
+  const { selectedSportsNavbarPage } = useSelectedPageContext();
   const currentMatch = React.useMemo(() => {
     return (matchData || []).filter((m) => {
       if (!m?.startTime) return false;
@@ -84,12 +120,12 @@ const AppContent: React.FC = () => {
   )
     return <Loader />;
   if (error) return <div>Error: {error.message}</div>;
-
   return (
     <main
       className="w-full mx-auto max-w-[640px] sm:max-w-[768px] md:max-w-[960px] lg:max-w-[1024px] xl:max-w-[1200px] 2xl:max-w-[1440px] 3xl:max-w-[1440px]"
       ref={containerRef}
     >
+      <Breadcrumbs sportName={selectedSportsNavbarPage} />
       <div>
         <SportSection
           title={`TÂM ĐIỂM ${
